0134efec972b3c1bf1edd426b81a02d059410edf
LuisFelipePoma <u202110902@upc.edu.pe>
2023-11-28 23:11:48 -0500

    changes to pc2 para el cabezon

--- src/week12-toyIR/sample.cpp
+++ src/week12-toyIR/sample.cpp
@@ -1,3 +1,5 @@
+#include <llvm-17/llvm/ADT/APInt.h>
+#include <llvm-17/llvm/IR/Value.h>
 #include <llvm/ADT/APFloat.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/IR/BasicBlock.h>
@@ -10,35 +12,56 @@
 #include <llvm/Support/raw_ostream.h>
 #include <memory>
 #include <vector>
-#include <iostream>
 #include <llvm/Support/TargetSelect.h>
+
+static auto TheContext = std::make_unique<llvm::LLVMContext>();
+static auto TheModule = std::make_unique<llvm::Module>("Mi cool jit", *TheContext);
+static auto TheBuilder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+
+void createFunction(const char *name, llvm::Type *, llvm::Value *);
 
 int main()
 {
-	// InitializeNativeTarget();
-	// InitializeNativeTargetAsmPrinter();
-	auto TheContext = std::make_unique<llvm::LLVMContext>();
-	auto TheModule = std::make_unique<llvm::Module>("Mi cool jit", *TheContext);
-	auto TheBuilder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+	// Types
+	auto typeDouble = llvm::Type::getDoubleTy(*TheContext);
+	auto typeInt = llvm::Type::getInt8Ty(*TheContext);
+	auto typeVoid = llvm::Type::getVoidTy(*TheContext);
 
-	std::cout << "visitPrintExpr\n";
-	std::vector<double> Args;
-	std::vector<llvm::Type *> Doubles(Args.size(),
-									  llvm::Type::getDoubleTy(*TheContext));
-	llvm::FunctionType *FT = llvm::FunctionType::get(
-		llvm::Type::getDoubleTy(*TheContext), Doubles, false);
+	// Values
+	llvm::Value *valInt = llvm::ConstantInt::get(*TheContext, llvm::APInt(8, 1));
+	llvm::Value *valDouble = llvm::ConstantFP::get(*TheContext, llvm::APFloat(1.2));
 
+	// init
+	createFunction("_main_void_", typeVoid, nullptr);
+	createFunction("_int_", typeInt, valInt);
+	createFunction("_double_", typeDouble, valDouble);
+
+	// NO andre no! 
+	TheModule->print(llvm::errs(), nullptr);
+
+	return 0;
+}
+
+void createFunction(const char *name, llvm::Type *types, llvm::Value *value)
+{
+	// Types
+	std::vector<double> args;
+	std::vector<llvm::Type *> Doubles(args.size(), llvm::Type::getDoubleTy(*TheContext));
+
+	// Function Args
+	llvm::FunctionType *functionTypes = llvm::FunctionType::get(
+		types,
+		Doubles,
+		false);
+
+	// Function Declaration
 	llvm::Function *F = llvm::Function::Create(
-		FT, llvm::Function::ExternalLinkage, "_anon_", TheModule.get());
+		functionTypes, llvm::Function::ExternalLinkage, name, TheModule.get());
 
+	// Entry
 	llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry", F);
 	TheBuilder->SetInsertPoint(BB);
 
-	auto numVal = 10.0;
-	llvm::Value *val = llvm::ConstantFP::get(*TheContext, llvm::APFloat(numVal));
-	
-	TheBuilder->CreateRet(val);
-
-	TheModule->print(llvm::errs(), nullptr);
-	return 0;
+	// Return value
+	TheBuilder->CreateRet(value);
 }
